fc37415dd65f9b5c189989c5137b7f9c660560fe
alonmergui2024 <alonmergi@haderahigh.org.il>
2024-01-15 13:49:45 +0200

    כ

--- q.py
+++ q.py
@@ -1,239 +1,208 @@
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
-import cv2
 import streamlit as st
+import requests
+import pandas as pd
+import time
+import datetime
+from streamlit_extras.switch_page_button import switch_page
+import random
+import json
+import os
 
-current_date = datetime.now()
-formatted_date = current_date.strftime("%Y%m%d")
-URL = "https://www.espn.com/nba/teams"
-header = {
-    'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
-}
-response = requests.get(URL, headers=header)
-s = BeautifulSoup(response.content, "html.parser")
-results = s.find(id="fitt-analytics")
+check = False
 
-
-class Autocomplete:
-    def __init__(self, team):
-        self.team = team
-
-    def suggest(self, prefix):
-        prefix = prefix.lower()
-        suggestions = []
-        for word in self.team:
-            for miniword in word.split(" "):
-                if miniword.lower().startswith(prefix.lower()) or miniword.lower() == prefix.lower():
-                    suggestions.append(word)
-                    break
-
-        return suggestions
+data = []
+a = False
 
 
-if results:
-    team = []
-    teams = results.find_all('h2', class_='di clr-gray-01 h5')
-    for i in teams:
-        team.append(i.text)
-    autocomplete_engine = Autocomplete(team)
-option = ["this week games", "all games"]
-autocomplete_engine1 = Autocomplete(option)
+st.set_page_config(
+    page_title="Stocks analyzer",
+    page_icon=r"icons8-stock-48.png",
 
-def register():
-    users = open("users.text", "r")
-    username = input("Create username: ")
-    password = input("Create password: ")
-    password1 = input("Confirm password: ")
-    d = []
-    f = []
-    g = []
-    for i in users:
-        if "," in i:
-            a, b, c = i.split(",")
-            b = b.strip()
-            d.append(a)
-            f.append(b)
-            g.append(c)
+)
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+
+def click_button():
+    st.session_state.clicked = True
+
+
+
+json_file_path = "users.json"
+
+
+def stockanalyzer():
+    st.title("Stock Analyzer")
+    company_name = st.text_input("Enter company name or item:")
+
+    min_date = datetime.date(2022, 1, 1)
+    max_date = datetime.datetime.now() - datetime.timedelta(days=16)
+    start_date = st.date_input("Select start date:",
+                               min_value=min_date,
+                               max_value=max_date,
+                               value=min_date)
+
+    end_date = datetime.datetime.now().date()
+
+    st.button('Analyze', on_click=click_button)
+    if st.session_state.clicked:
+        if company_name == "":
+            st.warning("You have to enter a stock or a company name.")
         else:
-            pass
-
-    if password != password1:
-        print("Passwords are not the same, please try again")
-        register()
-    else:
-        if len(password) < 6:
-            print("Password are too short, please try again")
-            register()
-        elif username in d:
-            print("Username already exist, please try again")
-            register()
-        else:
-            users = open("users.text", "a")
-            print("Success!\n\n")
-            print("Welcome " + username + ",")
-            choice = input("Want to choose your favorite team right now? ")
-            if choice.lower() == "yes":
-                suggestions1 = ""
-                while not suggestions1:
-                    fav = input("Enter your favorite team: ")
-                    suggestions1 = autocomplete_engine.suggest(fav)
-                    if suggestions1:
-                        users.write(username + "," + password + "," + suggestions1[0] + "\n")
-                        print("Favorite team updated successfully to", suggestions1[0])
-                        return suggestions1
-                    else:
-                        print("the team is not found. please check the team name...")
-                        register()
-
-            elif choice.lower() == "no":
-                print("Ok you can always do this later")
-                users.write(username + "," + password + "," + " " + "\n")
-                guest()
+            if company_name.upper() == "APPLE" or company_name.upper() == "AAPL" or company_name.upper() == "APLE":
+                stock_symbol = "AAPL"
+            elif company_name.upper() == "NVDA" or company_name.upper() == "NVIDIA" or company_name.upper() == "NVIDA":
+                stock_symbol = "NVDA"
             else:
-                print("Please enter only yes or no")
-                register()
-    guest()
+                with st.spinner("Fetching stock symbol..."):
+                    stock_symbol = get_stock_symbol(company_name)
+            if stock_symbol:
+                st.title("Stock Price Visualization App")
+                st.write(f"Displaying stock data for {stock_symbol}")
+
+                with st.spinner("Fetching stock data..."):
+                    stock_data = get_stock_data(stock_symbol, start_date, end_date)
+
+                if stock_data is not None:
+                    plot_stock_data(stock_data)
+                    lowest_point = stock_data['Close'].min()
+                    highest_point = stock_data['Close'].max()
+                    chart_data = pd.DataFrame({
+                        'Date': stock_data.index,
+                        'Stock Price': stock_data['Close'],
+                        'Lowest Point': lowest_point,
+                        'Highest Point': highest_point
+                    })
+                    st.line_chart(chart_data.set_index('Date'))
+                    st.success(f"Highest Stock Price: ${round(highest_point, 2)}")
+                    st.warning(f"Lowest Stock Price: ${round(lowest_point, 2)}")
+                    try:
+                        with st.spinner("Performing predictions..."):
+                            predicted_value_lr = predict_tomorrows_stock_value_linear_regression(stock_data)
+                            predicted_value_lstm = predict_tomorrows_stock_value_lstm(stock_data)
+                            time.sleep(1)
+
+                        st.write(f"Approximate tomorrow's stock value (Linear Regression): {predicted_value_lr:.2f}$")
+                        st.write(f"Approximate tomorrow's stock value (LSTM): {predicted_value_lstm:.2f}$")
+
+                        with st.expander("💡 What is LSTM?"):
+                            display_lstm_info()
+
+                        with st.expander("💡 What is Linear Regression?"):
+                            st.write("Linear Regression Simulation:")
+                            linear_Regression(stock_data)
 
 
 
-
-
-
-def access():
-    users = open("users.text", "r")
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    if len(username) > 0 and len(password) > 0:
-        d = []
-        f = []
-        g = []
-        for i in users:
-            if "," in i:
-                a, b, c = i.split(",")
-                b = b.strip()
-                d.append(a)
-                f.append(b)
-                g.append(c)
+                    except:
+                        st.warning("Not enough info for an AI approximation, please try an earlier date.")
             else:
-                pass
-        data = dict(zip(d, f))
-        data1 = dict(zip(data, g))
+                st.warning(f"Stock doesn't exist.\ntry again or check your input.")
 
-        try:
-            if data[username]:
+def user_exists(username):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            file_contents = file.read()
+            if file_contents:
                 try:
-                    if password == data[username]:
-                        print("Login success\n\n")
-                        print("Welcome back " + username + ",")
-
-                        if not data1[username]:
-                            choice = input(
-                                "It looks like you haven't set your favorite team. Do you want to do it now? ")
-                            if choice.lower().strip() == "yes":
-                                suggestions1 = ""
-                                while not suggestions1:
-                                    fav = input("Enter your favorite team: ")
-                                    suggestions1 = autocomplete_engine.suggest(fav)
-                                    if suggestions1:
-                                        users.write(username + "," + password + "," + suggestions1[0] + "\n")
-                                        data1[username] = suggestions1
-                                        with open("users.text", "w") as users_file:
-                                            for user, password, fav_team in zip(d, f, g):
-                                                users_file.write(user + "," + password + "," + suggestions1 + "\n")
-                                            print("Favorite team updated successfully to", suggestions1[0])
-                                            return suggestions1
-                                    else:
-                                        print("the team is not found. please check the team name...")
-                                        access()
-                            elif choice.lower().strip() == "no":
-                                print("Ok you can always do this later")
-                                users.write(username + "," + password + "," + " " + "\n")
-                                guest()
-                            else:
-                                print("Please write only yes or no")
-                                access()
-                        else:
-                            print("It looks like your favorite team is:", data1[username])
-                    else:
-                        print("Username or password incorrect")
-                        access()
-                except:
-                    print("Username or password incorrect")
-                    access()
+                    users = json.loads(file_contents)
+                except json.JSONDecodeError:
+                    st.error("Error decoding JSON. Please check the file format.")
+                    return False
             else:
-                print("Username or password incorrect")
-                access()
-        except:
-            print("Username or password incorrect")
-            access()
-    guest()
+                users = {}
+                with open(json_file_path, "w") as empty_file:
+                    json.dump(users, empty_file)
+    else:
+        users = {}
+    return username in users
 
 
-def home():
-    print("Signup | Login | guest ")
-    option = input()
-    if option.lower() == "login":
-        access()
-        var = access()
-        return var
-    elif option.lower() == "signup":
-        var = register()
-        return var
-    elif option.lower() == "guest":
-        guest()
+def sign_up(username, password, fav_team):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            file_contents = file.read()
+            if file_contents:
+                try:
+                    users = json.loads(file_contents)
+                except json.JSONDecodeError:
+                    st.error("Error decoding JSON. Please check the file format.")
+                    return
+            else:
+                users = {}
+    else:
+        users = {}
 
-fav = home()[0]
+    if username in users:
+        st.warning("Username is already taken. Please choose another one.")
+    else:
+        user_data = {"password": password, "fav_team": fav_team}
+        users[username] = user_data
+        with open(json_file_path, "w") as file:
+            json.dump(users, file)
+        st.success("You have successfully signed up!")
 
 
-def guest():
-    #if fav:
-     #   print("here are the games of your favorite team:", fav)
 
-    #else:
-        user_input = input("witch team do you want to see? ")
-        suggestions = autocomplete_engine.suggest(user_input.split(" ")[0])
 
-        if suggestions:
-            while len(suggestions) > 1:
-                print(suggestions)
-                user_input = input("witch team did you mean? ")
-                suggestions = autocomplete_engine.suggest(user_input)
-            print(suggestions[0].strip())
-            autocomplete_engine2 = Autocomplete(suggestions)
-            user_input1 = input("what do you want to see? (this week games or all games) ")
-            output = autocomplete_engine1.suggest(user_input1.split(" ")[0])
-            print(output[0].strip())
+# Function to sign in a user
+def Login(username, password):
+    if user_exists(username):
+        with open(json_file_path, "r") as file:
+            users = json.load(file)
+            user_data = users.get(username)
+            if user_data and user_data.get("password") == password:
+                fav_team = user_data.get("fav_team")
+                if fav_team:
+                    st.success(f"Welcome, {username}! favorite team: {fav_team}")
+                else:
+                    fav_team = "None"
+                    st.success(f"Welcome, {username}!")
+            else:
+                st.warning("Incorrect password. Please check for spelling and try again.")
+    else:
+        st.warning("User does not exist. Please sign up or check the username.")
 
-            if output[0].strip() == option[0]:
-                url = f"https://www.espn.com/nba/schedule/_/date/{formatted_date}"
-                response1 = requests.get(url, headers=header)
-                s1 = BeautifulSoup(response1.content, "html.parser")
-                results1 = s1.find(id="fittPageContainer")
-                table = results1.find_all("div", class_="ScheduleTables mb5 ScheduleTables--nba ScheduleTables--basketball")
-                teams1 = results1.find_all("td", class_="events__col Table__TD")
-                teams2 = results1.find_all("td", class_="colspan__col Table__TD")
-                times = results1.find_all("td", class_="date__col Table__TD")
-                games = list(zip(teams1, teams2, times))
-                table = [element for element in table if
-                         "teams__col Table__TD" and "teams__col Table__TD" and "leaders__col Table__TD" and "broadcast__col Table__TD" and "tickets__col Table__TD" not in element.attrs.get(
-                             "class", [])]
-                ''''
-                picture = results.find_all("img", class_="Image Logo Logo__lg")
-                for i in picture:
-                    print(i.img)
-                
-                for i in table:
-                    print(i.text.strip())
-                '''
-                for x, y, z in games:
-                    team1 = x.text.replace('@', '').strip().split()
-                    team2 = y.text.replace('@', '').strip().split()
-                    time = z.text.strip()
-                    suggestions2 = autocomplete_engine2.suggest(team1[0])
-                    suggestions3 = autocomplete_engine2.suggest(team2[0])
-                    if suggestions3 or suggestions2:
-                        g = f'{x.text}{y.text} {time}'
-                        print(g.replace('@', 'vs'))
-guest()
+def Account():
+    st.title("hi")
+
+def homepage():
+    st.title("BasketBall - Team Information")
+    want = st.button("hi")
+    if want:
+        switch_page("Login")
+    page = st.sidebar.radio("Navigation", ["Sign Up", "Login"])
+
+    if page == "Sign Up":
+        st.header("Sign Up")
+        username = st.text_input("Enter your username:")
+        password = st.text_input("Enter your password:", type="password")
+        fav_team = st.text_input("Enter your favorite team (optional):")
+        if st.button("Sign Up"):
+            sign_up(username, password, fav_team)
+            return username,password,fav_team
+
+    elif page == "Login":
+        st.header("Login")
+        username = st.text_input("Enter your username:")
+        password = st.text_input("Enter your password:", type="password")
+        if st.button("Login"):
+            if Login(username, password):
+                with open(json_file_path, "r") as file:
+                    users = json.load(file)
+                    user_data = users.get(username)
+                    fav_team = user_data.get("fav_team")
+                return username, password, fav_team
+            if username and password:
+                st.sidebar.radio("Select Page", ["Information", "real time stock investment"])
+
+
+
+page = st.sidebar.radio("Select Page", ["Home", "Information", "real time stock investment"])
+if page == "Home":
+    homepage()
+elif page == "Information":
+    pass
+elif page == "real time stock investment":
+    pass
